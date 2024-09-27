2ba426f8d6e261a90e19380076654111067402bd
albertlasalle <dalmaubravoalbert@gmail.com>
2024-09-27 18:57:10 +0200

    inici

---
+++ multiplesde3.py
@@ -0,0 +1,19 @@
+num = 0
+
+while num != 100:
+
+    num += 1
+
+    if num%3 == 0 and num%5 == 0:
+        print("FizzBuzz")
+
+    elif num%3 == 0:
+
+        print("Coca")
+
+    elif num%5 == 0:
+
+        print("Cola")
+
+    else:
+        print(num)
